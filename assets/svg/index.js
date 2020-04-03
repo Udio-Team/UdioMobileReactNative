31b62ef4add9dd57c145ce4ad2627cd127ea60a6
Alec Mather <aymather@gmail.com>
2020-04-03 10:52:20 -0700

    added basic file structure and babel plugin for absolute paths

---
+++ assets/svg/index.js
@@ -0,0 +1,6 @@
+export { default as tvIcon } from './tv.svg';
+export { default as profileIcon } from './profile.svg';
+export { default as searchIcon } from './search.svg';
+export { default as homeIcon } from './home.svg';
+export { default as udioIcon } from './udio.svg';
+export { default as bellIcon } from './bell.svg';
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [ 'module-resolver', {
+        alias: {
+          '$svg': './assets/svg',
+          '$images': './assets/images',
+          '$pages': './app/pages',
+          '$hooks': './utils/hooks',
+          '$context': './utils/context',
+          '$routes': './utils/routes.js',
+          '$components': './app/components'
+        }
+      }
+    ]
+]
 };
